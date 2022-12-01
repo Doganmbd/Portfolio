c440c25cae63deb0622a9988609fc61e45ed10cd
Mustafa Bilal Dogan <mbilaldogan90@gmail.com>
2022-12-01 15:07:20 +0300

    icons add

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,43 @@
-import "./Sidebar.scss";
+import './Sidebar.scss'
 
-import React from 'react';
-import { Link } from "react-router-dom";
-import LogoM from "../../assets/images/mbd.png";
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import LogoM from '../../assets/images/mbd.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
-    <div className = "side-bar">
+    <div className="side-bar">
       <Link className="logo" to="/">
         <img src={LogoM} alt="logo" />
-       </Link>
+      </Link>
+      <nav>
+        <NavLink
+          className="home-link"
+          exact="true"
+          activeclassname="active"
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} color="grey" />
+        </NavLink>
+        <NavLink
+          className="about-link"
+          exact="true"
+          activeclassname="active"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="grey" />
+        </NavLink>
+        <NavLink
+          className="contact-link"
+          exact="true"
+          activeclassname="active"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="grey" />
+        </NavLink>
+      </nav>
       SidebarIndex
     </div>
   )
